b2a6f88c0efce05912f051f99dc837a3bd72b4e4
kesavan-m-07 <kesavan.mathiyazhagan@numentica-ui.com>
2025-09-04 21:22:40 +0530

    Fixed the mentioned issues

--- DAY3/2_UniqueNumbers.js
+++ DAY3/2_UniqueNumbers.js
@@ -7,16 +7,16 @@ findDifference(arr1, arr2)
 // Output: [3, 42, 34, 35, -7, 87, 1]
 */
 
-const firstUserInput = [3, 45, 42, 11.00, 42];
-const secondUserInput = [35, -7, 87, 11.00, 1, 45];
+const firstUserInput = [3, 45, 42, 11.0, 42];
+const secondUserInput = [35, -7, 87, 11.0, 1, 45];
 
 //Check the input for number type
 const checkInputType = function (userInput) {
   let inputPointer = 0;
-  
+
   while (inputPointer < userInput.length) {
     const currentElement = userInput[inputPointer];
-    
+
     if (typeof currentElement !== "number") {
       console.error("invalid input type...");
       return false;
@@ -28,62 +28,59 @@ const checkInputType = function (userInput) {
 
 const findUniqueElements = function (firstUserInput, secondUserInput) {
   //Check if the inputs are array
-  if (
-   !Array.isArray(firstUserInput) || !Array.isArray(secondUserInput)
-  ) {
+  if (!Array.isArray(firstUserInput) || !Array.isArray(secondUserInput)) {
     console.error("Invalid Inputs Given...");
     return;
   }
 
   //Check if both inputs are not empty
   if (firstUserInput.length === 0 && secondUserInput.length === 0) {
-    return[];
+    return [];
   } else if (firstUserInput.length === 0 || secondUserInput.length === 0) {
-    if(firstUserInput.length === 0) return secondUserInput;
-    else if(secondUserInput === 0) return firstUserInput;
+    if (firstUserInput.length === 0) return secondUserInput;
+    else if (secondUserInput === 0) return firstUserInput;
   }
 
   //Check if both inputs are in numbers type
   const isFirstInputValid = checkInputType(firstUserInput);
-  
-  if (isFirstInputValid) {
-    const isSecondInputValid = checkInputType(secondUserInput);
-    if (!isSecondInputValid) {
-      return;
-    }
-  } else return;
+  if (!isFirstInputValid) {
+    return false;
+  }
+
+  const isSecondInputValid = checkInputType(secondUserInput);
+  if (!isSecondInputValid) {
+    return false;
+  }
 
   const mapToCheckPresence = {};
   const uniqueElements = [];
 
   for (let i = 0; i < firstUserInput.length; i++) {
     const currentElement = firstUserInput[i];
-    if(!mapToCheckPresence[currentElement]){
-        mapToCheckPresence[currentElement] = true;
+    if (!mapToCheckPresence[currentElement]) {
+      mapToCheckPresence[currentElement] = true;
     }
   }
 
   for (let i = 0; i < secondUserInput.length; i++) {
     const currentElement = secondUserInput[i];
-    if(mapToCheckPresence[currentElement]){
-        mapToCheckPresence[currentElement] = false;
-    }
-    else uniqueElements.push(currentElement); 
+    if (mapToCheckPresence[currentElement]) {
+      mapToCheckPresence[currentElement] = false;
+    } else uniqueElements.push(currentElement);
   }
 
-  for(let i=0;i<firstUserInput.length;i++){
+  for (let i = 0; i < firstUserInput.length; i++) {
     const currentElement = firstUserInput[i];
-    if(mapToCheckPresence[currentElement]){
-        uniqueElements.push(currentElement);
-        mapToCheckPresence[currentElement] = false;
+    if (mapToCheckPresence[currentElement]) {
+      uniqueElements.push(currentElement);
+      mapToCheckPresence[currentElement] = false;
     }
   }
 
   return uniqueElements;
-
 };
 
 const uniqueElements = findUniqueElements(firstUserInput, secondUserInput);
 if (uniqueElements !== undefined) {
-  console.log("Unique Elements: ",uniqueElements);
+  console.log("Unique Elements: ", uniqueElements);
 }
